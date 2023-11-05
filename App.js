d60b6652cd6061df4a52049e57925edb77215cae
Jackie <lcd.jackson@gmail.com>
2023-11-05 15:52:46 +0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import React, { useState } from "react";
+import { RadioButton } from "react-native-paper";
 
 export default function App() {
+  const [name, setName] = useState(null);
+  const [age, setAge] = useState(null);
+  const [likeCoffee, setLikeCoffee] = useState("Yes");
+
+  const handleSubmit = () => {
+    const message = `My name is ${name}. I am ${age} years old. I ${
+      likeCoffee === "Yes" ? "like" : "don't like"
+    } coffee.`;
+    Alert.alert("Summary", message);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Simple React Native Form</Text>
+
+
+      <Text style={styles.label}>Your Name:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        placeholderTextColor="green"
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+
+      <Text style={styles.label}>Your Age:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your age"
+        placeholderTextColor="green"
+        value={age}
+        onChangeText={(text) => setAge(text)}
+        keyboardType="numeric" // To show a numeric keyboard
+      />
+
+      <Text style={styles.label}>Like Coffee?</Text>
+      <RadioButton.Group
+        onValueChange={(value) => setLikeCoffee(value)}
+        value={likeCoffee}
+      >
+        <View style={styles.radioButtonContainer}>
+          <RadioButton.Item
+            label="Yes"
+            value="Yes"
+            color="green"
+            labelStyle={{ color: "green" }}
+            position="leading"
+            placeholderTextColor="green"
+          />
+        </View>
+        <View style={styles.radioButtonContainer}>
+          <RadioButton.Item
+            label="No"
+            value="No"
+            color="green"
+            labelStyle={{ color: "green" }}
+            position="leading"
+          />
+        </View>
+      </RadioButton.Group>
+
+      <Button title="Submit" onPress={handleSubmit} />
     </View>
   );
 }
@@ -13,8 +72,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    marginTop: 40,
+  },
+  label: {
+    color: "green",
+    fontSize: 25,
+    flexWrap: "wrap",
+    width: "100%",
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 5,
+    borderRadius: 1,
+    borderColor: "green", // Green border
+    color: "green",
+    fontStyle: "italic", // Italic style
+  },
+  radioButtonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
